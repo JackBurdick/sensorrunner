5004c6b6aef6618f099017b21d6ceb29cd5d42ab
Jack Burdick <jackbburdick@gmail.com>
2020-09-26 12:39:12 -0700

    led test

---
+++ main.py
@@ -0,0 +1,44 @@
+import typer
+from gpiozero.pins.mock import MockFactory
+
+from gpiozero import Device, DigitalOutputDevice
+from time import sleep
+
+# GPIO PINS: https://www.raspberrypi.org/documentation/usage/gpio/
+
+
+class Solenoid:
+    def __init__(self, pin_num):
+        self.pin_num = pin_num
+        self.out = DigitalOutputDevice(pin_num)
+
+    def spray(self):
+        print(f"spray {self.pin_num}")
+        self.out.on()
+        sleep(1)
+        self.out.off()
+
+
+class Solenoids:
+    def __init__(self):
+        self.S0 = Solenoid(23)
+        self.S1 = Solenoid(24)
+        self.S2 = Solenoid(17)
+        self.S3 = Solenoid(27)
+
+    def spray(self):
+        print(f"spray: {vars(self).keys()}")
+        for name, s in vars(self).items():
+            s.spray()
+
+
+def main(num: int, dev: bool = False):
+    typer.echo(f"Run {num}")
+    if dev:
+        Device.pin_factory = MockFactory()
+    solenoids = Solenoids()
+    solenoids.spray()
+
+
+if __name__ == "__main__":
+    typer.run(main)
