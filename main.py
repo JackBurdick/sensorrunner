2f945aa2f3a8edef783327b2ffce87d13ff111a0
Jack Burdick <jackbburdick@gmail.com>
2020-10-09 15:48:43 -0700

    allow connect/unconnect specification
    - add additional sleep in attempt to stabilize board on off

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from gpiozero.pins.native import NativeFactory
 """
 TODO:
 
+0. config specification
 1. logging
 2. organization
 3. db hookup
-4. config specification
 
 """
 
@@ -28,12 +28,20 @@ class Demux:
     # TODO: convert this to a baseclass
     """
 
-    def __init__(self, ordered_pins, pwr_pin, on_duration=1, stabilize_time=0.05):
+    def __init__(
+        self,
+        gpio_pins_ordered,
+        pwr_pin,
+        on_duration=1,
+        stabilize_time=0.05,
+        connected=None,
+        unconnected=None,
+    ):
         self.pwr = DigitalOutputDevice(pwr_pin)
         self.pwr.off()
 
         self.select = dict(
-            [(i, DigitalOutputDevice(p)) for i, p in enumerate(ordered_pins)]
+            [(i, DigitalOutputDevice(p)) for i, p in enumerate(gpio_pins_ordered)]
         )
         self._width = len(self.select)
 
@@ -42,6 +50,11 @@ class Demux:
                 f"on_duration ({on_duration}) must be type int or float not {type(on_duration)}"
             )
         self.on_duration = on_duration
+
+        self.connect_inds = self._define_connections(
+            gpio_pins_ordered, connected=connected, unconnected=unconnected
+        )
+        self.num_to_bin = self._num_to_bin(self.connect_inds)
 
         if not isinstance(stabilize_time, (int, float)):
             raise ValueError(
@@ -62,8 +75,10 @@ class Demux:
         if self.in_use:
             raise ValueError(f"can only be in use once at a time, currently in use!")
         self.zero()
-        self.pwr.off()  # TODO: maybe check first?
-        bin_rep = self._to_bin(num)
+        try:
+            bin_rep = self.num_to_bin[num]
+        except KeyError:
+            raise KeyError(f"num {num} is not available in {self.num_to_bin.keys()}")
         print(f"on: {bin_rep}")
         for i, v in enumerate(bin_rep[::-1]):
             if int(v):
@@ -74,21 +89,42 @@ class Demux:
 
     def _off_select(self, num):
         self.pwr.off()  # TODO: maybe check first?
-        bin_rep = self._to_bin(num)
+        bin_rep = self.num_to_bin[num]
         for i, v in enumerate(bin_rep[::-1]):
             if int(v):
                 self.select[i].off()
         self.in_use = False
 
     def zero(self):
-        # first turn power off to prevent accidental other sprays
+        # first turn power off to prevent accidental other runs
         self.pwr.off()
+        sleep(self.stabilize_time)
         for p in self.select.values():
             p.off()
         self.in_use = False
 
-    def spray_select(self, num, on_duration=None):
-        print(f"spray: {num}")  # {vars(self).keys()}
+    def _num_to_bin(self, nums):
+        num_bin = {}
+        for v in nums:
+            num_bin[v] = self._to_bin(v)
+        return num_bin
+
+    def _define_connections(self, index_pins, connected=None, unconnected=None):
+        avail_connects = len(index_pins) ** 2
+        if connected and unconnected:
+            raise ValueError(
+                f"please only specify connected ({connected}) or unconnected ({unconnected}), not both"
+            )
+        elif connected:
+            CONNECT_INDS = connected
+        elif unconnected:
+            CONNECT_INDS = [i for i in range(avail_connects) if i not in unconnected]
+        else:
+            CONNECT_INDS = [i for i in range(avail_connects)]
+        return CONNECT_INDS
+
+    def run_select(self, num, on_duration=None):
+        print(f"run: {num}")  # {vars(self).keys()}
         if on_duration:
             if not isinstance(on_duration, (int, float)):
                 raise ValueError(
@@ -106,7 +142,8 @@ class Demux:
 def main(num: int, dev: bool = False):
     INDEX_PINS = [25, 23, 24, 17]
     PWR_PIN = 27
-    # IND_SKIP = [] # TODO: allow for manual off of pins
+    UNCONNECTED = []  # TODO: allow for manual off of pins
+    CONNECTED = []
 
     typer.echo(f"Run {num}")
     if dev:
@@ -114,9 +151,9 @@ def main(num: int, dev: bool = False):
     else:
         Device.pin_factory = NativeFactory()
     sleep(1)
-    sprayer = Demux(INDEX_PINS, PWR_PIN)
+    sprayer = Demux(INDEX_PINS, PWR_PIN, connected=CONNECTED, unconnected=UNCONNECTED)
     for i in range(16):
-        sprayer.spray_select(i)
+        sprayer.run_select(i)
         sleep(0.5)
 
 
