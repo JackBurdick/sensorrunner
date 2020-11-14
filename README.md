ad7ac8d61a8b2889cfbd3638e551cfee5c6b2c6a
Jack Burdick <jackbburdick@gmail.com>
2020-11-14 14:39:53 -0800

    i2c mux

--- README.md
+++ README.md
@@ -16,3 +16,28 @@ pip install -U black
 pip install gpiozero pigpio
 
 ```
+
+
+
+## tof
+
+https://learn.adafruit.com/adafruit-vl53l0x-micro-lidar-distance-sensor-breakout/python-circuitpython
+
+```bash
+pip install adafruit-circuitpython-vl53l0x
+pip install adafruit-circuitpython-tca9548a
+```
+
+### GPIO
+# https://www.raspberrypi.org/documentation/usage/gpio/
+```bash
+pinout
+```
+
+### scan all ic devices
+
+scan ic devices
+```bash
+# allow i2c: sudo raspi-config
+i2cdetect -y 1
+```
--- tof.py
+++ tof.py
@@ -4,23 +4,42 @@ import board
 import busio
 
 import adafruit_vl53l0x
+import adafruit_tca9548a
 
-# https://learn.adafruit.com/adafruit-vl53l0x-micro-lidar-distance-sensor-breakout/python-circuitpython
-# scan ic devices
-# i2cdetect -y 1
-# allow i2c: sudo raspi-config
 
 # Initialize I2C bus and sensor.
 i2c = busio.I2C(board.SCL, board.SDA)
-vl53 = adafruit_vl53l0x.VL53L0X(i2c)
-vl53.measurement_timing_budget = 200000  # favor accuracy
+
+# mux
+tca = adafruit_tca9548a.TCA9548A(i2c)
+
+
+# each sensor
+vl53_a = adafruit_vl53l0x.VL53L0X(tca[0])
+vl53_a.measurement_timing_budget = 200000
+
+vl53_b = adafruit_vl53l0x.VL53L0X(tca[1])
+vl53_b.measurement_timing_budget = 200000
 # The default timing budget is 33ms
 
 MM_TO_INCH = 0.0393701
 
-for i in range(100):
-    try:
-        print(f"Range: {vl53.range * MM_TO_INCH: 0.3f} in")
-    except OSError:
-        pass
-    time.sleep(1.0)
+
+def main(num: int = 100):
+    for i in range(num):
+        try:
+            a_in = f"{vl53_a.range * MM_TO_INCH: 0.3f}"
+        except OSError:
+            a_in = None
+            pass
+        try:
+            b_in = f"{vl53_b.range * MM_TO_INCH: 0.3f}"
+        except OSError:
+            b_in = None
+            pass
+        print(f"dist. a:{a_in} b: {b_in}")
+        time.sleep(1.0)
+
+
+if __name__ == "__main__":
+    typer.run(main)
