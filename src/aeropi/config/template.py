77be534bd66060656037af91cf92de0077aa31e1
Jack Burdick <jackbburdick@gmail.com>
2020-11-23 19:26:40 -0800

    Correct address/channel template type

--- src/aeropi/config/template.py
+++ src/aeropi/config/template.py
@@ -12,8 +12,12 @@ TEMPLATE = {
     # TODO: include task information
     KPH("I2C", exact=True): {
         KPH("name", multi=True): {
-            "channel": Numeric(required=True, to_lower=True),
-            "address": Numeric(required=True, to_lower=True),
+            "channel": Numeric(
+                required=True, bounds=(0, 7), bounds_inclusive=(True, True)
+            ),
+            "address": Numeric(
+                required=True, bounds=(0x70, 0x77), bounds_inclusive=(True, True)
+            ),
             "device": Text(required=True, is_in_list=I2C_DEVICES),
         }
     }
