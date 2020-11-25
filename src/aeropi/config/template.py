455f5177edfafb176883680be1244710fbf6a1e0
Jack Burdick <jackbburdick@gmail.com>
2020-11-24 19:58:59 -0800

    Include basic demux template

--- src/aeropi/config/template.py
+++ src/aeropi/config/template.py
@@ -1,24 +1,60 @@
 from crummycm.validation.types.placeholders.placeholder import KeyPlaceholder as KPH
 from crummycm.validation.types.values.element.numeric import Numeric
+from crummycm.validation.types.values.compound.multi import Multi
 from crummycm.validation.types.values.element.text import Text
 
 # from crummycm.validation.types.values.element.bool import Bool
-# from crummycm.validation.types.values.compound.multi import Multi
+
 
 I2C_DEVICES = ["vl53l0x", "si7021"]
 
 
 TEMPLATE = {
     # TODO: include task information
-    KPH("I2C", exact=True): {
+    KPH("I2C", exact=True, required=False): {
         KPH("name", multi=True): {
             "channel": Numeric(
                 required=True, is_type=int, bounds=(0, 7), bounds_inclusive=(True, True)
             ),
             "address": Numeric(
-                required=True, is_type=int,  bounds=(0x70, 0x77), bounds_inclusive=(True, True)
+                required=True,
+                is_type=int,
+                bounds=(0x70, 0x77),
+                bounds_inclusive=(True, True),
             ),
             "device": Text(required=True, is_in_list=I2C_DEVICES),
         }
-    }
+    },
+    KPH("demux_name", required=False, ends_with="_demux"): {
+        "init": {
+            "gpio_pins_ordered": Multi(
+                element_types=Numeric(
+                    required=True,
+                    is_type=int,
+                    bounds=(0, 26),
+                    bounds_inclusive=(True, True),
+                ),
+                required=True,
+                homogeneous=True,
+            ),
+            "pwr_pin": Numeric(
+                required=True,
+                is_type=int,
+                bounds=(0, 26),
+                bounds_inclusive=(True, True),
+            ),
+            "on_duration": Numeric(required=True, is_type=float, bounds=(0, 5)),
+        },
+        "devices": {
+            KPH("name", multi=True): {
+                "index": Numeric(
+                    required=True,
+                    is_type=int,
+                    bounds=(0, 15),
+                    bounds_inclusive=(True, True),
+                ),
+                "on_duration": Numeric(required=True, is_type=float, bounds=(0, 5)),
+            }
+        },
+    },
 }
