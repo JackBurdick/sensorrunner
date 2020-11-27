9e392c1fef41179b848be0f8f75414d473da9e46
Jack Burdick <jackbburdick@gmail.com>
2020-11-27 10:12:38 -0800

    Print celeyconf, ensure parse generate

--- src/aeropi/celery_app.py
+++ src/aeropi/celery_app.py
@@ -1,10 +1,21 @@
 import celery
 import celeryconf
-
+import crummycm as ccm
+from aeropi.config.template import TEMPLATE
 
 app = celery.Celery("celery_run")
 
+print(celeryconf)
+
 app.config_from_object(celeryconf)
+
+out = ccm.generate(
+    "/home/pi/dev/aeropi/scratch/config_run/configs/basic_i2c.yml", TEMPLATE
+)
+print("=====" * 8)
+print(out)
+print("=====" * 8)
+
 # app.autodiscover_tasks(["tasks.demux.tasks", "tasks.iic.tasks"], force=True)
 
 
