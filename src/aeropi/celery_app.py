445cd68b83dff7b12acfe8df5a868a52fedb5054
Jack Burdick <jackbburdick@gmail.com>
2020-11-27 10:02:32 -0800

    Remove custom arg attempt until 5.0.3
    Will wait until 5.0.3 https://github.com/celery/celery/milestone/32,  relevant: https://github.com/celery/celery/pull/6501 and 
    https://github.com/celery/celery/issues/6307

--- src/aeropi/celery_app.py
+++ src/aeropi/celery_app.py
@@ -1,52 +1,13 @@
 import celery
-from celery import bootsteps
-
-from celery.bin.base import CeleryOption
 import celeryconf
-import click
+
 
 app = celery.Celery("celery_run")
-
-# https://github.com/celery/celery/blob/07000d826573a97ff633b688bda7bf30db114dfe/docs/userguide/extending.rst
-# def add_worker_arguments(parser):
-#     parser.add_argument("--rawconfig", default=None)
-
-
-app.user_options["worker"].add(
-    click.option("--rawconfig", default=None, help="Config File Required")
-)  # add_worker_arguments
-
-
-class MyBootstep(bootsteps.Step):
-    def __init__(self, parent, rawconfig=False, **options):
-        super().__init__(parent, **options)
-        if rawconfig:
-            print(rawconfig)
-        else:
-            raise ValueError(f"no rawconfig specified")
-
-
-app.steps["worker"].add(MyBootstep)
-
-# # https://stackoverflow.com/questions/21365101/celery-worker-and-command-line-args
-# app.user_options["worker"].add(
-#     CeleryOption("--rawconfig", default=None, help="Config File Required")
-# )
-
-
-# class CustomArgs(bootsteps.Step):
-#     def __init__(self, worker, rawconfig=None, **options):
-#         if rawconfig is not None:
-#             print(rawconfig)
-#         else:
-#             raise ValueError(f"no rawconfig specified")
-
-
-# app.steps["worker"].add(CustomArgs)
 
 app.config_from_object(celeryconf)
 # app.autodiscover_tasks(["tasks.demux.tasks", "tasks.iic.tasks"], force=True)
 
 
 # aeropi/scratch/config_run/configs/basic_i2c.yml
+# possibly helpful later:
 # https://gist.github.com/chenjianjx/53d8c2317f6023dc2fa0
