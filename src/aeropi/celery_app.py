4ca536edd3e797cc48e8a92ba3c6f55d42414470
Jack Burdick <jackbburdick@gmail.com>
2020-11-27 09:28:29 -0800

    Update custom arg logic to match docs

--- src/aeropi/celery_app.py
+++ src/aeropi/celery_app.py
@@ -6,21 +6,40 @@ import celeryconf
 
 app = celery.Celery("celery_run")
 
-# # https://stackoverflow.com/questions/21365101/celery-worker-and-command-line-args
-app.user_options["worker"].add(
-    CeleryOption("--config_file", default=None, help="Config File Required")
-)
+# https://github.com/celery/celery/blob/07000d826573a97ff633b688bda7bf30db114dfe/docs/userguide/extending.rst
+def add_worker_arguments(parser):
+    parser.add_argument("--rawconfig", default=None, help="Config File Required"),
 
 
-class CustomArgs(bootsteps.Step):
-    def __init__(self, worker, config_file=None, **options):
-        if config_file is not None:
-            print(config_file)
+app.user_options["worker"].add(add_worker_arguments)
+
+
+class MyBootstep(bootsteps.Step):
+    def __init__(self, parent, rawconfig=False, **options):
+        super().__init__(parent, **options)
+        if rawconfig:
+            print(rawconfig)
         else:
-            raise ValueError(f"no config_file specified")
+            raise ValueError(f"no rawconfig specified")
 
 
-app.steps["worker"].add(CustomArgs)
+app.steps["worker"].add(MyBootstep)
+
+# # https://stackoverflow.com/questions/21365101/celery-worker-and-command-line-args
+# app.user_options["worker"].add(
+#     CeleryOption("--rawconfig", default=None, help="Config File Required")
+# )
+
+
+# class CustomArgs(bootsteps.Step):
+#     def __init__(self, worker, rawconfig=None, **options):
+#         if rawconfig is not None:
+#             print(rawconfig)
+#         else:
+#             raise ValueError(f"no rawconfig specified")
+
+
+# app.steps["worker"].add(CustomArgs)
 
 app.config_from_object(celeryconf)
 # app.autodiscover_tasks(["tasks.demux.tasks", "tasks.iic.tasks"], force=True)
