dd68c3e34fe94afe25b1605756842d0f8784fd79
Jack Burdick <jackbburdick@gmail.com>
2020-11-28 10:45:52 -0800

    Update trial run

--- scratch/config_run/run.py
+++ scratch/config_run/run.py
@@ -1,6 +1,17 @@
+import crummycm as ccm
+from aeropi.config.template import TEMPLATE
 from aeropi.run.run import build_devices_from_config
+from aeropi.secrets import L_CONFIG_DIR, P_CONFIG_DIR
+
+try:
+    out = ccm.generate(L_CONFIG_DIR, TEMPLATE)
+except FileNotFoundError:
+    out = ccm.generate(P_CONFIG_DIR, TEMPLATE)
+USER_CONFIG = out
 
 # print("start")
-build_devices_from_config("./configs/basic_i2c.yml")
-# print("done")
-
+conf = ccm.generate(USER_CONFIG, TEMPLATE)
+print(conf)
+print("-----" * 8)
+devs = build_devices_from_config(conf)
+print(devs)
