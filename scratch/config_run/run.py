5cd4ca35c28aea30730f889ae49c08e915a19029
Jack Burdick <jackbburdick@gmail.com>
2020-12-05 11:17:34 -0800

    Attempt redbeat script

--- scratch/config_run/run.py
+++ scratch/config_run/run.py
@@ -1,15 +1,13 @@
+from aeropi.user_config import USER_CONFIG
 import celery
 from redbeat import RedBeatSchedulerEntry as Entry
 
 import time
-import crummycm as ccm
 from aeropi.celery_app import setup_app
-from aeropi.config.template import TEMPLATE
 from aeropi.run.run import build_devices_from_config, build_task_params_from_config
-from aeropi.secrets import L_CONFIG_DIR, P_CONFIG_DIR
 
 app = setup_app()
-print('now,now')
+
 
 def create_task_entries(task_params):
     entries = {}
@@ -41,16 +39,8 @@ def delete_entries(entries):
 
 
 if __name__ == "__main__":
-    try:
-        out = ccm.generate(L_CONFIG_DIR, TEMPLATE)
-    except FileNotFoundError:
-        out = ccm.generate(P_CONFIG_DIR, TEMPLATE)
-    USER_CONFIG = out
-
-    # print("start")
-    conf = ccm.generate(USER_CONFIG, TEMPLATE)
     # devs = build_devices_from_config(conf)
-    task_params = build_task_params_from_config(conf)
+    task_params = build_task_params_from_config(USER_CONFIG)
     entries = create_task_entries(task_params)
     print(entries)
 
--- src/aeropi/user_config.py
+++ src/aeropi/user_config.py
@@ -5,5 +5,5 @@ from aeropi.secrets import REDIS_GLOBAL_host, REDIS_GLOBAL_port, REDIS_GLOBAL_db
 
 r = redis.Redis(host=REDIS_GLOBAL_host, port=REDIS_GLOBAL_port, db=REDIS_GLOBAL_db)
 
-config_location = r.get("USER_CONFIG_LOCATION").decode('utf-8')
+config_location = r.get("USER_CONFIG_LOCATION").decode("utf-8")
 USER_CONFIG = ccm.generate(config_location, TEMPLATE)
