227b9faf7a77b98e17ec81cbcd56ca15b92d4322
Jack Burdick <jackbburdick@gmail.com>
2020-12-07 17:02:06 -0800

    Remove `+`

--- src/aeropi/run/daemonize_cmd.sh
+++ src/aeropi/run/daemonize_cmd.sh
@@ -99,7 +99,7 @@ CELERYD_GROUP="pi"
 CELERY_CREATE_DIRS=1
 
 # add z opt to celeryd
-CELERYD_OPTS=$CELERY_Z_OPT+$CELERYD_WORKER_OPTS
+CELERYD_OPTS="$CELERY_Z_OPT $CELERYD_WORKER_OPTS"
 
 #celerybeat
 #CELERYBEAT_OPTS="-Z '/home/pi/dev/aeropi/scratch/config_run/configs/basic_i2c.yml'"
