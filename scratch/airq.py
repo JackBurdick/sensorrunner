e2508f35489511fc6252b33fb7c4e91b084d31fe
Jack Burdick <jackbburdick@gmail.com>
2020-12-09 09:00:21 -0800

    Average data, use power pin
    The device runs the fan nonstop, will use a pin to cut power to device when not in use. Will also take 3 measurements and average the results. NOTE: the sensor requires a 30sec initialization and the sample freq is 2.3 seconds and so there is a delay (5 sec in this case, for two cycles) before readings

--- scratch/airq.py
+++ scratch/airq.py
@@ -1,11 +1,17 @@
 import time
 
 import typer
+from collections import Counter
 
 from adafruit_pm25.i2c import PM25_I2C
 import adafruit_tca9548a
 import board
 import busio
+
+from gpiozero import Device, DigitalOutputDevice
+from gpiozero.pins.native import NativeFactory
+
+Device.pin_factory = NativeFactory()
 
 # Initialize I2C bus and sensor.
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -13,6 +19,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # mux
 tca2 = adafruit_tca9548a.TCA9548A(i2c, address=0x72)
 
+pwr_pin = DigitalOutputDevice(6)
 
 # each sensor
 aq_sensor = PM25_I2C(tca2[2])
@@ -26,13 +33,30 @@ aq_sensor = PM25_I2C(tca2[2])
 # sampling rate 2.3 seconds
 
 
-def main(num: int = 100):
-    for i in range(num):
-        try:
-            aqdata = aq_sensor.read()
-        except RuntimeError:
-            aqdata = None
+def average_dict(v_dicts):
+    # filter values with None
+    for k_d, v_d in v_dicts.items():
+        if v_d:
+            v_dicts[k_d] = {k: v for k, v in v_d.items() if v is not None}
 
+    # calculate average
+    occurance, sums = Counter(), Counter()
+    for n, sensor_ret_d in v_dicts.items():
+        sums.update(sensor_ret_d)
+        occurance.update(itemset.keys())
+
+    avg_res = {x: float(sums[x]) / counters[x] for x in sums.keys()}
+    return avg_res
+
+
+def parse_sensor(aq_sensor):
+    try:
+        aqdata = aq_sensor.read()
+    except RuntimeError:
+        aqdata = None
+
+    vals = {}
+    if aqdata:
         # keys
         # reading key, unit, new_name
         # Particle Matter concentrations of different size particles in micro-gram per cubic meter
@@ -51,13 +75,37 @@ def main(num: int = 100):
             ("pm100 env", "ug/m^3", "pm100"),
         ]
 
-        vals = {}
         for skt in sensor_keys:
             vals[skt[2]] = (aqdata[skt[0]], skt[1])
+    return vals
 
-        print(vals)
 
-        time.sleep(1.0)
+def main(num: int = 100):
+    for i in range(num):
+        NUM_ITERATIONS = 3
+
+        # turn on
+        pwr_pin.on()
+        print("initializing...")
+        time.sleep(30)
+        print("ready")
+
+        raw_vals = {}
+        for i in range(NUM_ITERATIONS):
+            cur_v = parse_sensor(aq_sensor)
+            print("----------" * 8)
+            print("IT: {i}")
+            print(cur_v)
+            time.sleep(4)
+            raw_vals[f"{i}"] = cur_v
+
+        print("----------" * 8)
+        avg_vals = average_dict(raw_vals)
+        print(avg_vals)
+
+        pwr_pin.off()
+        time.sleep(15)
+        print("sleeping")
 
 
 if __name__ == "__main__":
