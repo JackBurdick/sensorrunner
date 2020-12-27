f569a8f6cfa192c809e9a8b7b386f7b7f96f61d7
Jack Burdick <jackbburdick@gmail.com>
2020-12-27 10:50:21 -0800

    Update ulog

--- scratch/micro_cam/ulog.py
+++ scratch/micro_cam/ulog.py
@@ -9,10 +9,31 @@ class ULog:
         logging.basicConfig(level=logging.DEBUG)
         logger = logging.getLogger(__name__)
         self.logger = logger
+        self.count = 0
+
+    def _log(self, msg, level, time):
+        self.d[self.count] = {"msg": msg, "level": level, "time": time}
+        self.count += 1
 
     def debug(self, msg):
         self.logger.debug(msg)
-        self.d[time.time()] = msg
+        mc_time = time.time()
+        self._log(msg, "debug", mc_time)
+
+    def info(self, msg):
+        self.logger.info(msg)
+        mc_time = time.time()
+        self._log(msg, "info", mc_time)
+
+    def warning(self, msg):
+        self.logger.warning(msg)
+        mc_time = time.time()
+        self._log(msg, "warning", mc_time)
+
+    def error(self, msg):
+        self.logger.warning(msg)
+        mc_time = time.time()
+        self._log(msg, "error", mc_time)
 
     def to_file(self, fpath):
         with open(fpath, "w") as outfile:
