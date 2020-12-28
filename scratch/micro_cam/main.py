7bc5ac1f33f465846268718f03d574801b9936a2
Jack Burdick <jackbburdick@gmail.com>
2020-12-28 15:28:37 -0800

    update server

--- scratch/micro_cam/main.py
+++ scratch/micro_cam/main.py
@@ -8,9 +8,21 @@ import gc
 import json
 from ulog import ULog
 
-IMG_PATH_TEMPLATE = "sd/{}__{}.jpg"
+# import machine
+
+
+IMG_PATH_TEMPLATE = "sd/{}__{}__{}.jpg"
+V1_BASE_PATH = "/api/v1"
+BASE_SD_DIR = "/sd"
+IMG_API_PATH = "{}/{}".format(V1_BASE_PATH, "retrieve")
 
 logger = ULog("main.py")
+
+# LED = machine.Pin(1, machine.Pin.OUT)
+# time.sleep_ms(300)
+# logger.info("led set to pin 1")
+# LED.off()
+# logger.info("led.off")
 
 # TODO: check if connected
 sd = setup_sd(logger)
@@ -27,20 +39,25 @@ app = tinyweb.webserver()
 logger.debug("app webserver created")
 
 
-def _write_image(cams):
+def _write_image(cam_info):
     global CAM
     ret = {}
-    if cams:
-        for _, cam_info in cams.items():
-            buf = CAM.capture()
-            img_path = IMG_PATH_TEMPLATE.format(cam_info["index"], cam_info["ts"])
-            f = open(img_path, "w")
-            f.write(buf)
-            time.sleep_ms(100)
-            f.close()
-            ret["path"] = img_path
-            ret["index"] = cam_info["index"]
-            ret["ts"] = cam_info["ts"]
+    if cam_info:
+        # LED.on()
+        # time.sleep_ms(500)
+        buf = CAM.capture()
+        img_path = IMG_PATH_TEMPLATE.format(
+            cam_info["bucket"], cam_info["index"], cam_info["ts"]
+        )
+        f = open(img_path, "w")
+        f.write(buf)
+        time.sleep_ms(100)
+        f.close()
+        # LED.off()
+        ret["path"] = img_path
+        ret["bucket"] = cam_info["bucket"]
+        ret["index"] = cam_info["index"]
+        ret["ts"] = cam_info["ts"]
     return ret
 
 
@@ -54,6 +71,8 @@ def _basic_parse_qs(raw_qs):
 
 
 class Status:
+    url = "{}/{}".format(V1_BASE_PATH, "status")
+
     def get(self, data):
         mem = {
             "mem_alloc": gc.mem_alloc(),
@@ -72,22 +91,26 @@ class Status:
 
 
 class Capture:
+    url = "{}/{}".format(V1_BASE_PATH, "capture")
+
     def post(self, data):
         img_cap = None
         if data:
             try:
-                cams = data["cams"]
+                cam_info = data["cam"]
             except KeyError:
-                cams = None
+                cam_info = None
             # cams: {index: ___, ts: ___}
-            img_cap = _write_image(cams)
-        resp = {"cams": img_cap}
+            img_cap = _write_image(cam_info)
+        resp = {"cam": img_cap}
         return resp, 200
 
 
 class SDFiles:
+    url = "{}/{}".format(V1_BASE_PATH, "sdfiles")
+
     def get(self, data):
-        files = uos.listdir("/sd")
+        files = uos.listdir(BASE_SD_DIR)
         # remove dotfiles
         files = [f for f in files if not f.startswith(".")]
         # {"files": ["now_2.jpg", "0_12_26_2020__11_43_10.jpg"]}
@@ -95,26 +118,27 @@ class SDFiles:
         return resp, 400
 
     def delete(self, data):
-        # e.g. "/sd/now_2.jpg"
+        # e.g. "now_2.jpg"
         try:
             f_name = data["file_name"]
         except KeyError:
             f_name = None
         if f_name:
+            f_path = "{}/{}".format(BASE_SD_DIR, f_name)
             try:
-                f = open(f_name, "r")
+                f = open(f_path, "r")
             except OSError:
-                resp = ({"message": "file not found"}, 400)
+                resp = ({"message": "file not found: {}".format(f_path)}, 400)
             else:
                 f.close()
-                uos.remove("{}".format(f_name))
-                resp = ({"message": "file deleted"}, 200)
+                uos.remove("{}".format(f_path))
+                resp = ({"message": "file deleted: {}".format(f_path)}, 200)
         else:
-            resp = ({"message": "no `file_name` detected"}, 400)
+            resp = ({"message": "no `file_name` detected. data: {}".format(data)}, 400)
         return resp[0], resp[1]
 
 
-@app.route("/retrieve/")
+@app.route(IMG_API_PATH)
 async def images(req, resp):
     qs = req.query_string
     try:
@@ -131,15 +155,24 @@ async def images(req, resp):
             index = qs_d["index"]
         except KeyError:
             index = None
+        try:
+            bucket = qs_d["bucket"]
+        except KeyError:
+            bucket = None
     else:
         ts = None
         index = None
+        bucket = None
 
-    if index is not None and ts is not None:
-        fp = IMG_PATH_TEMPLATE.format(index, ts)
+    if bucket is not None and index is not None and ts is not None:
+        fp = IMG_PATH_TEMPLATE.format(bucket, index, ts)
         await resp.send_file(fp, content_type="image/jpeg")
     else:
-        msg = {"message": "index ({}) or ts ({}) missing".format(index, ts)}
+        msg = {
+            "message": "bucket ({}) index ({}) or ts ({}) missing: qs_d:{}".format(
+                bucket, index, ts, qs_d
+            )
+        }
         resp.code = 400
         resp.add_header("Content-Type", "application/json")
         msg_json = json.dumps(msg)
@@ -148,14 +181,20 @@ async def images(req, resp):
         await resp.send(msg_json)
 
 
+logger.debug("add resource: {}".format(IMG_API_PATH))
+
+
 def run():
-    logger.debug("adding resources to app")
-    app.add_resource(Status, "/api/status")
-    logger.debug("add resource: /api/status")
-    app.add_resource(Capture, "/capture")
-    logger.debug("add resource: /capture")
-    app.add_resource(SDFiles, "/sd_files")
-    logger.debug("add resource: /sd_files")
+
+    app.add_resource(Status, Status.url)
+    logger.debug("add resource: {}".format(Status.url))
+
+    app.add_resource(Capture, Capture.url)
+    logger.debug("add resource: {}".format(Capture.url))
+
+    app.add_resource(SDFiles, SDFiles.url)
+    logger.debug("add resource: {}".format(SDFiles.url))
+
     logger.debug("calling app run")
     logger.to_file("/sd/mylog.json")
     app.run(host="0.0.0.0", port=8081)
