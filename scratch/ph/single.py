581cf1072a12c5fef32283f5e0e977d2cebd50a9
Jack Burdick <jackbburdick@gmail.com>
2021-02-21 14:21:48 -0800

    Attempt open on the fly

--- scratch/ph/single.py
+++ scratch/ph/single.py
@@ -15,8 +15,9 @@ class AtlasResponse:
         return str(self.__class__.__name__) + ": " + f"{self.__dict__}"
 
 
-class TCA9548A(object):
-    def __init__(self, name, address, bus_num):
+class AtlasMux(object):
+    # TCA9548A
+    def __init__(self, name, address, bus_num=1):
         self.name = name
         self.address = address
         self.bus_num = bus_num
@@ -38,9 +39,9 @@ class TCA9548A(object):
             file=f"/dev/i2c-{self.bus_num}", mode="rb", buffering=0
         )
 
-        I2C_rep = 0x703
-        fcntl.ioctl(self.file_read, I2C_rep, address)
-        fcntl.ioctl(self.file_write, I2C_rep, address)
+        _I2C_rep = 0x703
+        fcntl.ioctl(self.file_read, _I2C_rep, address)
+        fcntl.ioctl(self.file_write, _I2C_rep, address)
 
     def read_value(self):
         cmd = "R"
@@ -53,13 +54,39 @@ class TCA9548A(object):
         ar.data = resp[1:].strip().strip(b"\x00")
         return ar
 
+    def read_sensor(self, channel=3, address=0x63, reading_delay_s=1.5):
+        self.set_channel(channel)
+
+        _I2C_rep = 0x703
+        with io.open(file=f"/dev/i2c-{self.bus_num}", mode="wb", buffering=0) as wfp:
+            fcntl.ioctl(wfp, _I2C_rep, address)
+            cmd = "R"
+            cmd += "\00"
+            wfp.write(cmd.encode("latin-1"))
+
+        time.sleep(reading_delay_s)
+
+        ar = AtlasResponse()
+        with io.open(file=f"/dev/i2c-{self.bus_num}", mode="rb", buffering=0) as rfp:
+            fcntl.ioctl(rfp, _I2C_rep, address)
+            resp = rfp.read(31)
+            ar.status_code = resp[0]
+            ar.data = resp[1:].strip().strip(b"\x00")
+
+        # set all channels off
+        self.bus.write_byte(self.address, 0)
+
+        return ar
+        #####
+
 
 MUX_ADDR = 0x70
-
-tca = TCA9548A("atlas_mux", MUX_ADDR, 1)
-tca.set_channel(3)
-tca._atlas_setup()
-
-v = tca.read_value()
-print(v)
-print("-----" * 8)
+ph_addr = 0x63
+tca = AtlasMux("atlas_mux", MUX_ADDR)
+# tca.set_channel(3)
+# tca._atlas_setup()
+for i in range(10):
+    v = tca.read_sensor(channel=3, address=0x63)
+    print(f"{i}: {v}")
+    print("-----" * 8)
+    time.sleep(1)
