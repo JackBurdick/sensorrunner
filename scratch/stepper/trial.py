55af4f38d12dcfefb85fb3d95a4f893b98f949bb
Jack Burdick <JackBurdick@users.noreply.github.com>
2021-03-06 11:08:37 -0800

    Use gpiozero

--- scratch/stepper/trial.py
+++ scratch/stepper/trial.py
@@ -1,42 +1,56 @@
 import sys
-import RPi.GPIO as gpio 
 import time
 
+from gpiozero import Device, DigitalOutputDevice
 
-gpio.setmode(gpio.BCM)
+# from gpiozero.pins.mock import MockFactory
+from gpiozero.pins.native import NativeFactory
 
-r_steps = 400
-l_steps = 450
+Device.pin_factory = NativeFactory()
+
+
+class Stepper:
+    """stepper"""
+
+    def __init__(
+        self,
+        # init params
+        init_config,
+        name="stepper",
+    ):
+        self.name = name
+        self.dir_pin = DigitalOutputDevice(init_config["dir"])
+        self.step_pin = DigitalOutputDevice(init_config["step"])
+        self.pulse_width = 0.0015
+        self.time_between = 0.005
+
+    def step(self):
+        self.step_pin.on()
+        time.sleep(self.pulse_width)
+        self.step_pin.off()
+
+    def move_direction(self, num_steps, direction):
+        cur_step = 0
+        if direction:
+            self.dir_pin.on()
+        else:
+            self.dir_pin.off()
+
+        while cur_step < num_steps:
+            self.step()
+            print(f"{cur_step}/{num_steps} {direction}")
+            cur_step += 1
+            time.sleep(self.time_between)
 
 
 DIRPIN = 27
 STEPIN = 17
-gpio.setwarnings(False)
-gpio.setup(DIRPIN, gpio.OUT)
-gpio.setup(STEPIN, gpio.OUT)
+init_config = {"dir": DIRPIN, "step": STEPIN}
+s = Stepper(init_config=init_config)
 
-L_DIR = True
-R_DIR = False
+r_steps = 400
+l_steps = 450
 
-time.sleep(0.5)
-
-
-def move_direction(num_steps, direction):
-    time_between = 0.005
-    pulse_time = 0.0015
-    cur_step = 0
-    gpio.output(DIRPIN, direction)
-    while cur_step < num_steps:
-        gpio.output(STEPIN, True)
-        time.sleep(pulse_time)
-        gpio.output(STEPIN, False)
-        print(f"{num_steps} {direction}")
-        cur_step += 1
-        time.sleep(time_between)
-
-
-move_direction(l_steps, L_DIR)
+s.move_direction(l_steps, True)
 time.sleep(0.3)
-move_direction(r_steps, R_DIR)
-
-gpio.cleanup()
+s.move_direction(r_steps, False)
