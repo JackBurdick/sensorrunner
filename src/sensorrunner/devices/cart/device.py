addb4273903f43869240ddb3f9daeb146d084e6a
Jack Burdick <JackBurdick@users.noreply.github.com>
2021-08-05 17:38:16 -0700

    Create device.py

---
+++ src/sensorrunner/devices/cart/device.py
@@ -0,0 +1,7 @@
+from calcatrix.devices.multiview import MultiView  # pylint: disable=import-error
+from calcatrix.functions.photo import take_photo  # pylint: disable=import-error
+
+
+class Cart:
+    def __init__(self, init_config):
+        pass
